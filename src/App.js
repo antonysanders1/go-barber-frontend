def69d401639260a63a7362714212f77173051ad
antony_sanders <antony.devon92@gmail.com>
2020-08-27 14:39:07 -0400

    imported connect and barbers container to pass props thru to BarbersContainer from the store

--- src/App.js
+++ src/App.js
@@ -1,19 +1,27 @@
 import React, {Component} from 'react';
+import {connect} from 'react-redux';
+import BarbersContainer from './containers/BarbersContainer';
 
 
 class App extends Component {
 
   componentDidMount() {
-    fetch('http://localhost:3000/api/v1/customers/1/appointments')
-    .then(res => res.json())
-    .then(data => console.log(data[0].barber.name))
   }
 
   render(){
     return (
-      <div className="App"></div>
+      <div className="App">
+        App
+        <BarbersContainer/>
+      </div>
     )
   }
 }
 
-export default App;
+// const mapStateToProps = (state) => {
+//   return {
+//     barber: state.barber
+//   }
+// }
+
+export default connect()(App);

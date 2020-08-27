bf79bd90cc32d04bd58bfa7d0547c010c6c5c90e
antony_sanders <antony.devon92@gmail.com>
2020-08-27 02:05:57 -0400

    1. imported redux, react-redux, and redux-thunk -- 2. created a store variable in index.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
 import React, {Component} from 'react';
-import './App.css';
+
 
 class App extends Component {
+
+  componentDidMount() {
+    fetch('http://localhost:3000/api/v1/customers/1/appointments')
+    .then(res => res.json())
+    .then(data => console.log(data[0].barber.name))
+  }
+
   render(){
     return (
       <div className="App"></div>
--- src/index.js
+++ src/index.js
@@ -2,12 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import {createStore, applyMiddleware, compose} from 'redux'
+import {Provider} from 'react-redux'
+import thunk from 'redux-thunk'
+
+const composeEnhancers = window.REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const store = createStore(rootReducer, composeEnhancers(applyMiddleware(thunk)))
 
 ReactDOM.render(
   <React.StrictMode>
+    <Provider store={store}> 
     <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
-
-

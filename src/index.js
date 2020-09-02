3d142c8f8f8815e4aef038a26d7f99d501f95606
antony_sanders <antony.devon92@gmail.com>
2020-09-02 03:03:09 -0400

    Updated store and imported ThemeProvider from Material UI

--- src/index.js
+++ src/index.js
@@ -1,21 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import {createStore, applyMiddleware, compose} from 'redux'
-import {Provider} from 'react-redux'
+import { ThemeProvider } from '@material-ui/core/styles'
+import theme from './theme'
+import App from './App.js';
+import { createStore, applyMiddleware, compose} from 'redux'
+import { Provider } from 'react-redux'
 import thunk from 'redux-thunk'
-import barberReducer from './reducers/barberReducer'
+import rootReducer from './reducers/rootReducer'
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
-const store = createStore(barberReducer, composeEnhancers(applyMiddleware(thunk)))
+const store = createStore(rootReducer, composeEnhancers(applyMiddleware(thunk)))
 
-ReactDOM.render(
-  <React.StrictMode>
-    <Provider store={store}> 
-    <App />
-    </Provider>
-  </React.StrictMode>,
-  document.getElementById('root')
+ReactDOM.render( <React.StrictMode>
+        <Provider store = { store }>
+            <ThemeProvider theme = { theme }>
+                <App/>
+            </ThemeProvider>
+        </Provider> 
+    </React.StrictMode>,
+    document.getElementById('root')
 );
